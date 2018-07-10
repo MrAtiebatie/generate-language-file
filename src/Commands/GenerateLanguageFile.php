afcf4cbd8892cf0853d3ffb1b0af5ce17ca7824c
Sjors van Dongen <sitesjors@hotmail.com>
2018-07-10 14:54:50 +0200

    First commit

---
+++ src/Commands/GenerateLanguageFile.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Mratiebatie\GenerateLanguageFile\Commands;
+
+use Illuminate\Console\Command;
+
+class GenerateLanguageFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate-language-file';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the language files for JavaScript';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $lang = config('app.locale');
+
+        $files   = glob(resource_path('lang/' . $lang . '/*.php'));
+        $strings = [];
+
+        foreach ($files as $file) {
+            $name           = basename($file, '.php');
+            $strings[$name] = require $file;
+        }
+
+        // Generate JavaScript line
+        $javascript = 'window.i18n = ' . json_encode($strings) . ';';
+
+        if (file_put_contents(base_path('public/js/lang.js'), $javascript)) {
+            $this->info('Language file generated...');
+        } else {
+            $this->error('Error generating language file...');
+        }
+    }
+}
---
+++ src/LanguageServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\GenerateLanguageFile;
+
+class LanguageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                GenerateLanguageFile::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
